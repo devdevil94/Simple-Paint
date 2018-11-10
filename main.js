fdc4251afaec089c6677c297760307899920b5b3
Saud <saudd2gh@gmail.com>
2018-11-10 13:42:42 +0400

    testing the draw method

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
 
 
 var canvas = document.querySelector('canvas');
-var cx = canvas.getContext('2d');
+var cxt = canvas.getContext('2d');
 
-cx.fillStyle = "#FF0000";
-cx.fillRect(0, 0, 150, 75);
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+
+
+test();
+
+
+
+
+
+
+
+
+function test(){
+	for(var i=0; i < 90; i++) 
+		addClick(i,i,true);
+
+	draw();
+}
+
+function addClick(x, y, dragging){
+	clickX.push(x);
+	clickY.push(y);
+	clickDrag.push(dragging);
+}
+
+function draw(){
+	cxt.clearRect(0, 0, cxt.canvas.width, cxt.canvas.height);
+
+	cxt.strokeStyle = "#000000";
+	cxt.lineJoin = "round";
+	cxt.lineWidth = 5;
+
+	for(var i=0; i < clickX.length; i++) {		
+    	cxt.beginPath();
+
+	    if(clickDrag[i] && i){
+			cxt.moveTo(clickX[i-1], clickY[i-1]);
+		}else{
+			cxt.moveTo(clickX[i]-1, clickY[i]);
+		}
+
+		cxt.lineTo(clickX[i], clickY[i]);
+		cxt.closePath();
+		cxt.stroke();
+  	}
+}
